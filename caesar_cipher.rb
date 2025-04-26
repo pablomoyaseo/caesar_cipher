c71ff9a5c8c60635c61c93ed2199e1c27010d196
pablomoyaseo <ing.pmoya@gmail.com>
2025-04-25 20:54:36 -0400

    Caesar_cipher , we use (a .. z), chars to convert in array, map to iterate every char, join to creat a string

---
+++ caesar_cipher.rb
@@ -0,0 +1,19 @@
+
+
+def caesar_cipher(string, number)
+  alphabet = ('a'..'z').to_a
+  encrypted = string.chars.map do |char|
+    if alphabet.include?(char.downcase)  
+      original_index = alphabet.index(char.downcase)
+      new_index = (original_index + number) % 26  
+      char == char.downcase ? alphabet[new_index] : alphabet[new_index].upcase
+    else
+      char  
+    end
+  end
+  encrypted.join
+end
+
+puts caesar_cipher('y', 5)  # => "ifmmp"
+puts caesar_cipher('What a string!', 5)  # => "Mjqqt, Btwqi!"
+
